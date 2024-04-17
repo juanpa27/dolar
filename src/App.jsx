e4725720acbf53605cd14db7c31b26924d711b09
jbellenzier <jbellenzier@gmail.com>
2024-04-17 08:48:08 -0400

    nuevo modo dark en tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 
-import './App.css'
+import { IconContext } from "react-icons";
+import './App.css' 
+import React from "react";
+import { IoMoon } from "react-icons/io5"; 
+import { IoSunny } from "react-icons/io5";
 
 function App() {
-  
+  const [dark, setDark] = React.useState(false);
+
+    const darkModeHandler = () => {
+        setDark(!dark);
+        document.body.classList.toggle("dark");
+    }
 
   return (
     <>
-      <div className='items-center'>
-        <button className="btn btn-blue">Dark Mode</button> 
-
-        <h1 className="text-6xl font-bold">DARK MODE</h1>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  
-        </p>
-
-      </div>
+    
+    <div className="bg-yellow-500 dark:bg-black">
+            <button onClick={()=> darkModeHandler()}>
+                {
+                    
+                    dark && <IoSunny />
+                }
+                {
+                    !dark && <IoMoon />
+                }
+            </button>
+        </div>
     </>
   )
 }
